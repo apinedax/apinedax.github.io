8784f9ccd3d3f73c130d2d6213afd3d2acded813
apinedax <me@pinedax.dev>
2023-12-18 22:11:05 -0500

    configuring jekyll

---
+++ _posts/2023-12-18-hola-mundo.markdown
@@ -0,0 +1,7 @@
+---
+title:  "Hola Mundo!"
+---
+
+Bievenido a mi primer post!
+
+Hola, quizas me has visto en alguna presentación, pero ahora estoy buscando enfrentarme a uno de mis mayores retos que es escribir!
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hola Mundo!",
+        "excerpt":"Bievenido a mi primer post!   Hola, quizas me has visto en alguna presentación, pero ahora estoy buscando enfrentarme a uno de mis mayores retos que es escribir!  ","categories": [],
+        "tags": [],
+        "url": "/2023/12/18/hola-mundo.html",
+        "teaser": null
+      }]
